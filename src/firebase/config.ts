7a0f7355b13c95275422096eee11323bebdb067d
Pensare Marketing <pensaremarketing@gmail.com>
2026-01-16 12:21:12 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,2 +1,9 @@
 // This file will be populated with your Firebase project's configuration.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-8244745229-f670f",
+  "appId": "1:208916538255:web:92297ce9ad79657bab21fa",
+  "apiKey": "AIzaSyCj-SMFJzHcTEm0wrjtKhZc-3dvq42Hoc4",
+  "authDomain": "studio-8244745229-f670f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "208916538255"
+};
