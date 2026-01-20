1c40b8bab63d36fc0424a01375ee854b8b0dcda6
Pensare Marketing <pensaremarketing@gmail.com>
2026-01-20 06:22:34 +0000

    dropdown click issue

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -120,7 +120,7 @@ const CommandItem = React.forwardRef<
       e.preventDefault();
     }}
     className={cn(
-      "relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground",
       className
     )}
     {...props}
